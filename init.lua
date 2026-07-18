6773b738efda25c8b47a4b4978c73cfcebcb59dd
Kirill Siukhin <kirillsiukhin@proton.me>
2026-07-18 18:07:41 +0500

    enter insert mode on terminal start

--- init.lua
+++ init.lua
@@ -219,6 +219,13 @@ do
   -- or just use <C-\><C-n> to exit terminal mode
   vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+  -- Automatically enter insert mode when opening a terminal
+  vim.api.nvim_create_autocmd('TermOpen', {
+    group = vim.api.nvim_create_augroup('AutoInsertTerminal', { clear = true }),
+    pattern = '*',
+    command = 'startinsert',
+  })
+
   -- TIP: Disable arrow keys in normal mode
   -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
   -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
