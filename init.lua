874b52ceda7cecb1199174c946067b28a423b00c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-05 21:46:56 +0500

    fix indentation

--- init.lua
+++ init.lua
@@ -158,6 +158,11 @@ do
   vim.o.list = true
   vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 2
+  vim.opt.shiftwidth = 2
+  vim.opt.softtabstop = 2
+
   -- Preview substitutions live, as you type!
   vim.o.inccommand = 'split'
 
